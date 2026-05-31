067ff26008c31547343afc4cbcb0d7d3e4770900
Busra-Dereli <84290091+Busra-Dereli@users.noreply.github.com>
2026-05-31 15:53:20 +0300

    Edge.cs: Source alani eklendi, constructor guncellendi

--- DataStructers/Edge.cs
+++ DataStructers/Edge.cs
@@ -2,14 +2,18 @@ using System;
 
 namespace PCBBaglantiAgiOptimizasyonu
 {
+    // Proje Ekibi:
+    // Busra Dereli
     public class Edge 
     {
+        public Node Source;      
         public Node Destination; 
         public int Weight;       
         public Edge Next;
 
-        public Edge(Node destination, int weight = 1) 
+        public Edge(Node source, Node destination, int weight = 1) 
         {
+            this.Source = source;
             this.Destination = destination;
             this.Weight = weight;
             this.Next = null;
